eccb271f52ef77c41746808acfa8d736b50cdc64
Jesse Farnham <jessefarnham1@gmail.com>
2020-01-28 10:11:25 -0500

    Add missing files

---
+++ code/flightinfo.js
@@ -0,0 +1,85 @@
+const aws = require('aws-sdk')
+const dynamo = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'})
+
+const tableName = process.env.aircraftTableName
+
+function update(evt, ctx, cb) {
+    const item = JSON.parse(evt.body)
+    dynamo.put({
+        Item: item,
+        TableName: tableName},
+        (err, resp) => {
+            if (err) {
+                cb(err)
+            }
+            else {
+                cb(null, {
+                    statusCode: 200,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    body: JSON.stringify(resp)
+                })
+            }
+        }
+    )
+}
+
+function get(evt, ctx, cb) {
+    const tailNumber = evt.pathParameters.tailnumber
+    dynamo.get(
+        {
+            Key: {
+                tailNumber: tailNumber
+            },
+            TableName: tableName
+        },
+        (err, data) => {
+            if (err) {
+                cb(err)
+            }
+            else {
+                const flightInfo = data.Item
+                cb(null, {
+                    statusCode: 200,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    body: JSON.stringify(flightInfo)
+                })
+            }
+        }
+    )
+}
+
+function list(evt, ctx, cb) {
+    dynamo.scan(
+        {TableName: tableName},
+        (err, data) => {
+            if (err) {
+                cb(err)
+            }
+            else {
+                const flightInfos = data.Items
+                cb(null, {
+                    statusCodes: 200,
+                    headers : {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    body: JSON.stringify(flightInfos)
+                })
+            }
+        }
+    )
+
+
+}
+
+module.exports = {
+    update,
+    get,
+    list
+}
