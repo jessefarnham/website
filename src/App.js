c48911549748cc261f784fa8f5627830c8f5b2aa
Jesse Farnham <jessefarnham1@gmail.com>
2020-04-02 13:38:34 -0400

    simplify navbar

--- src/App.js
+++ src/App.js
@@ -9,20 +9,13 @@ function App(props) {
   return (
     <div className="App Container">
         <Navbar fluid collapseOnSelect>
-            <Navbar.Brand>
-              <Link to='/'>Home</Link>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse>
-                <Nav pullRight>
-                    <LinkContainer to='/flightstatus'>
-                        <Nav.Link>Flight Status</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to='/contact'>
-                        <Nav.Link>Contact</Nav.Link>
-                    </LinkContainer>
-                </Nav>
-            </Navbar.Collapse>
+          <Link to='/'>Home</Link>
+            <LinkContainer to='/flightstatus'>
+                <Nav.Link>Flight Status</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/contact'>
+                <Nav.Link>Contact</Nav.Link>
+            </LinkContainer>
         </Navbar>
         <Routes />
     </div>
