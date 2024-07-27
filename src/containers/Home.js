e9b8fbd4e9f2ba989c403cd5651284d0f375bf63
Jesse Farnham <jessefarnham@Jesses-MacBook-Air.local>
2024-07-27 15:46:14 -0400

    update homepage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -9,14 +9,16 @@ export default function Home() {
                 <h2>Jesse Farnham, Ph.D.</h2>
                 <h4>Professional overview</h4>
                 <p>I am a software engineer working in the finance industry.
-                   Starting April 2020, I will be working as a lead quantitative
-                   developer on the commodities team at { }
+                   I am currently a lead quantitative developer on the commodities team at { }
                    <a href="http://www.citadel.com">Citadel</a> in Greenwich, CT.
                    I work primarily in Python and have experience with Scala.
-                   My primary engineering interests are systems and API design.
+                   My primary engineering interests are systems and API design;
+		   my current business focus is the modeling and pricing of 
+                   exotic structured commodity derivatives. Previously, I worked
+		   on market analytics for natural gas in North America.
                 </p>
                 <p>
-                  I began my professional software engineering career
+                  I began my software engineering career
                   as a quantitative developer at <a href="http://www.aqr.com">AQR Capital</a>.
                   Previously, I interned at Google,
                   where I prototyped a biological data standardization pipeline.
@@ -37,26 +39,27 @@ export default function Home() {
                 <h4>Technology Interests</h4>
                 <p>
                 I am always interested in improving my knowledge of the technology world.
-                Most recently I have been learning about web development by building this
-                website. I have also been practicing C++, graphics, and game development
-                by creating a <a href="http://en.wikipedia.org/wiki/Roguelike">roguelike</a> { }
-                video game.
+		Most recently I have been learning about low-level software engineering by
+		attempting to implement <a href="https://github.com/jessefarnham/apple2/blob/main/main.s">
+		Tetris in Apple 6502 assembly language</a> 
+	        and by studying the 
+		<a href="https://www.amazon.com/Lions-Commentary-Unix-John/dp/1573980137">
+		source code of the UNIX operating system</a>.
                 </p>
                 <h4>Other Interests</h4>
                 <p>
                 My main non-work interests involve aviation and being outdoors. I own and fly
                 a 1946 Cessna 140, "The Firefly;" click "Flight Status" above to see where I
-                have flown recently. I also enjoy powered paragliding, flying R/C gliders,
+                have flown recently. I also enjoy flying R/C gliders,
                 birding, hiking, backpacking, mountainbiking, scuba diving,
                 winter sports,
-                and trying to keep up with my fianc&eacute;e's knowledge of geology and
-                environmental science.
+                and trying unsuccessfully to beat my wife in board games and Rummy.
                 </p>
                 <img src={FireflyInPA} alt="The Firefly on the ground"
                  className="img-fluid"/>
                 <hr/>
                 <footer>
-                This page was last modified on April 3, 2020.
+                This page was last modified on July 27, 2024.
                 </footer>
             </div>
         </div>
