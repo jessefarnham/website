e3dba6fe5a095a15207c7d6c3c8b8b705591f38c
Jesse Farnham <jessefarnham1@gmail.com>
2025-10-31 08:01:19 -0400

    Update main page, add warp.md

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -13,9 +13,9 @@ export default function Home() {
                    <a href="http://www.citadel.com">Citadel</a> in Greenwich, CT.
                    I work primarily in Python and have experience with Scala.
                    My primary engineering interests are systems and API design;
-		   my current business focus is the modeling and pricing of
-                   exotic structured commodity derivatives. Previously, I worked
-		   on market analytics for natural gas in North America.
+		   my current business focus is on market analytics for natural gas in North America.
+          Previously, I worked on the modeling and pricing of
+                   exotic structured commodity derivatives.
                 </p>
                 <p>
                   I began my software engineering career
@@ -48,18 +48,19 @@ export default function Home() {
                 </p>
                 <h4>Other Interests</h4>
                 <p>
-                My main non-work interests involve aviation and being outdoors. I own and fly
-                a 1946 Cessna 140, "The Firefly;" click "Flight Status" above to see where I
-                have flown recently. I also enjoy flying R/C gliders,
-                birding, hiking, backpacking, mountainbiking, scuba diving,
+                Outside of work, I enjoy spending time with my beautiful wife,
+                Cathy, and trying unsuccessfully to beat her in board games
+                and Rummy. My main personal interests involve aviation and being outdoors. 
+                I own and fly a 1946 Cessna 140, "The Firefly;" click "Flight Status" above 
+                to see where I have flown recently. I also enjoy flying R/C gliders,
+                birding, hiking, backpacking, mountainbiking, scuba diving, and
                 winter sports,
-                and trying unsuccessfully to beat my wife in board games and Rummy.
                 </p>
                 <img src={FireflyInPA} alt="The Firefly on the ground"
                  className="img-fluid"/>
                 <hr/>
                 <footer>
-                This site was last modified on October 16, 2024.
+                This site was last modified on October 31, 2025.
                 </footer>
             </div>
         </div>
